e60f4b1ed0049c8f21d7f5ab84090ab49f6a9e1e
vishaldhull <vinsdeck@gmail.com>
2020-07-07 13:35:53 +0530

    Added the login and logout method toedit or delete the blogs.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -13,19 +14,21 @@ def post_detail(request, pk):
 	#post = Post.objects.get(pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
 
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now()		//To save blogs as draft
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form':form})
 
+@login_required
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
@@ -33,9 +36,25 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+			#post.published_date = timezone.now() 		//To save blogs as draft
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+	post=get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('post_list')
